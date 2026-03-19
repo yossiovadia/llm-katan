18632ffe2eda76ee01b354f15860b37bd326473b
Yossi Ovadia <yovadia@redhat.com>
2026-03-19 13:48:38 -0700

    Bump version to 0.5.2

--- llm_katan/__init__.py
+++ llm_katan/__init__.py
@@ -11,7 +11,7 @@ try:
 
     __version__ = version("llm-katan")
 except PackageNotFoundError:
-    __version__ = "0.5.1"
+    __version__ = "0.5.2"
 
 __author__ = "Yossi Ovadia"
 __email__ = "yovadia@redhat.com"
--- llm_katan/server.py
+++ llm_katan/server.py
@@ -23,7 +23,7 @@ try:
 
     __version__ = version("llm-katan")
 except PackageNotFoundError:
-    __version__ = "0.5.1"
+    __version__ = "0.5.2"
 
 logger = logging.getLogger(__name__)
 
